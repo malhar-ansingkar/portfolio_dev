028824458388fb7556bdbff1ef147c43c3a7a150
malhar <malharansingkar7057@gmail.com>
2024-02-19 17:56:54 +0530

    added loader

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Experience from "./components/experience/experience";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { useEffect, useState } from "react";
 import { loadSlim } from "@tsparticles/slim";
+import Loading from "./components/loading/Loading";
 
 function App() {
   const [init, setInit] = useState(false);
@@ -15,15 +16,9 @@ function App() {
   // this should be run only once per application lifetime
   useEffect(() => {
     initParticlesEngine(async (engine) => {
-      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-      // starting from v2 you can add only the features you need reducing the bundle size
-      //await loadAll(engine);
-      //await loadFull(engine);
       await loadSlim(engine);
-      //await loadBasic(engine);
     }).then(() => {
-      setInit(true);
+      setTimeout(setInit(true), 1000);
     });
   }, []);
 
@@ -33,97 +28,103 @@ function App() {
 
   return (
     <>
-      <div className="App">
-        <Navbar />
-        <Home />
-        <About />
-        <Experience />
-        <Projects />
-        <Contact />
-        {/* <Footer /> */}
-      </div>
-      {init && (
-        <Particles
-          id="tsparticles"
-          particlesLoaded={particlesLoaded}
-          options={{
-            background: {
-              color: {
-                value: "#ffffff",
-              },
-            },
-            fpsLimit: 120,
-            particles: {
-              number: {
-                value: 190,
-                density: {
-                  enable: true,
-                  value_area: 700,
+      {init ? (
+        <div className="App">
+          <Navbar />
+          <Home />
+          <About />
+          <Experience />
+          <Projects />
+          <Contact />
+          {init && (
+            <Particles
+              id="tsparticles"
+              particlesLoaded={particlesLoaded}
+              options={{
+                background: {
+                  color: {
+                    value: "#ffffff",
+                  },
                 },
-              },
-              color: {
-                value: "#000000",
-              },
-              shape: {
-                type: "triangle",
-                stroke: {
-                  width: 0,
-                  color: "#000000",
+                fpsLimit: 120,
+                particles: {
+                  number: {
+                    value: 190,
+                    density: {
+                      enable: true,
+                      value_area: 700,
+                    },
+                  },
+                  color: {
+                    value: "#000000",
+                  },
+                  shape: {
+                    type: "triangle",
+                    stroke: {
+                      width: 0,
+                      color: "#000000",
+                    },
+                    polygon: {
+                      nb_sides: 5,
+                    },
+                    image: {
+                      src: "img/github.svg",
+                      width: 100,
+                      height: 100,
+                    },
+                  },
+                  opacity: {
+                    value: 0.5,
+                    random: false,
+                    anim: {
+                      enable: false,
+                      speed: 1,
+                      opacity_min: 0.1,
+                      sync: false,
+                    },
+                  },
+                  size: {
+                    value: 3,
+                    random: true,
+                    anim: {
+                      enable: false,
+                      speed: 40,
+                      size_min: 0.1,
+                      sync: false,
+                    },
+                  },
+                  links: {
+                    enable: true,
+                    distance: 130,
+                    color: "#000000",
+                    opacity: 0.7,
+                    width: 1,
+                  },
+                  move: {
+                    enable: true,
+                    speed: 3,
+                    direction: "none",
+                    random: false,
+                    straight: false,
+                    out_mode: "out",
+                    bounce: false,
+                    attract: {
+                      enable: false,
+                      rotateX: 600,
+                      rotateY: 1200,
+                    },
+                  },
+                  detectRetina: true,
                 },
-                polygon: {
-                  nb_sides: 5,
-                },
-                image: {
-                  src: "img/github.svg",
-                  width: 100,
-                  height: 100,
-                },
-              },
-              opacity: {
-                value: 0.5,
-                random: false,
-                anim: {
-                  enable: false,
-                  speed: 1,
-                  opacity_min: 0.1,
-                  sync: false,
-                },
-              },
-              size: {
-                value: 3,
-                random: true,
-                anim: {
-                  enable: false,
-                  speed: 40,
-                  size_min: 0.1,
-                  sync: false,
-                },
-              },
-              links: {
-                enable: true,
-                distance: 130,
-                color: "#000000",
-                opacity: 0.7,
-                width: 1,
-              },
-              move: {
-                enable: true,
-                speed: 3,
-                direction: "none",
-                random: false,
-                straight: false,
-                out_mode: "out",
-                bounce: false,
-                attract: {
-                  enable: false,
-                  rotateX: 600,
-                  rotateY: 1200,
-                },
-              },
-              detectRetina: true,
-            },
-          }}
-        />
+              }}
+            />
+          )}
+        </div>
+      ) : (
+        <div className="loading-container">
+          <Loading />
+          <h2>Loading...</h2>
+        </div>
       )}
     </>
   );
